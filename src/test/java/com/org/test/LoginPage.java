c4b374d48db8488247b9e4f77715119d06d88dfc
Sudheer M T <sudt1216@netcracker.com>
2020-01-08 16:41:33 +0530

    search page is added

--- src/test/java/com/org/test/LoginPage.java
+++ src/test/java/com/org/test/LoginPage.java
@@ -1,5 +1,12 @@
 package com.org.test;
 
+import org.testng.annotations.Test;
+
 public class LoginPage {
+	@Test
+	public void main() {
+		System.out.println("hello world");
+		System.out.println("colt");
+	}
 
 }
---
+++ src/test/java/com/org/test/SearchPage.java
@@ -0,0 +1,5 @@
+package com.org.test;
+
+public class SearchPage {
+
+}
